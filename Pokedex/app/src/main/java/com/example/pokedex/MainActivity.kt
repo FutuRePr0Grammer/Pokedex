c40dd96467a94cf61923745149ed77f6247d6619
David Rochon <drochon59@yahoo.com>
2023-06-29 11:59:24 -0400

    Got basic UI design done with preview and composables in MainActivity.kt

--- Pokedex/app/src/main/java/com/example/pokedex/MainActivity.kt
+++ Pokedex/app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -4,6 +4,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 import retrofit2.Call
 import retrofit2.Callback
@@ -18,7 +32,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         RetrofitApplication.instance.retrofitComponent.inject(this)
         setContent {
-
+            Column() {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    pokemonCardComposable()
+                    pokemonCardComposable()
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    pokemonCardComposable()
+                    pokemonCardComposable()
+                }
+            }
         }
 
 
@@ -33,5 +56,41 @@ class MainActivity : ComponentActivity() {
 
         })
 
+
+
+    }
+    @Composable
+    fun pokemonCardComposable() {
+        Box(
+            modifier = Modifier
+                .padding(
+                start = 16.dp,
+                end = 16.dp,
+                top = 16.dp,
+                bottom = 8.dp
+                )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .background(Color.Red)
+                    .size(150.dp)
+            ) {
+
+                Column {
+                    Text("Pokemon Name")
+                    Text("Pokemon Type(s)")
+                }
+                Text("Image")
+            }
+        }
+    }
+
+
+
+    @Preview
+    @Composable
+    fun pokemonCardComposablePreview() {
+        pokemonCardComposable()
     }
 }
