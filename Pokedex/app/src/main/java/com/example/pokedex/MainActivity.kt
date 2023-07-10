c96988e7b9860abcbe4a70fc4ce6e8fe0d6d6e4a
David Rochon <drochon59@yahoo.com>
2023-07-10 11:39:34 -0400

    Made sprite images fill the bounds so the images can scale up and fillMaxSize can be used. Not perfect, images are too long, may need to push them below the names in another row or column to make it work because the names squish them and reduce the max width.

--- Pokedex/app/src/main/java/com/example/pokedex/MainActivity.kt
+++ Pokedex/app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -13,8 +13,13 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Text
@@ -23,6 +28,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -135,8 +141,9 @@ class MainActivity : ComponentActivity() {
                             .decoderFactory(SvgDecoder.Factory())
                             .build(),
                         contentDescription = null,
+                        contentScale = ContentScale.FillBounds,
                         modifier = Modifier
-                            .size(160.dp)
+                            .fillMaxSize(100f)
                     )
                 }
             }
