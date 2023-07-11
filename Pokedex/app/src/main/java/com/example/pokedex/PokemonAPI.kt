65267da04beb1030c4c8ee8539db10d0972d0f74
David Rochon III <drochon59@yahoo.com>
2023-07-11 15:11:03 +0000

    List screen lists pokemon with their types and image, and the color changes depending on the first type for each pokemon

--- Pokedex/app/src/main/java/com/example/pokedex/PokemonAPI.kt
+++ Pokedex/app/src/main/java/com/example/pokedex/PokemonAPI.kt
@@ -1,9 +1,16 @@
 package com.example.pokedex
 
+import com.example.pokedex.network.dto.PokemonDetailResponseDto
+import com.example.pokedex.network.dto.PokemonListResponseDto
+
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PokemonAPI {
     @GET("pokemon")
-    fun getPokemon(): Call<List<Pokemon>>
+    fun getPokemon(): Call<PokemonListResponseDto>
+
+    @GET("pokemon/{id}")
+    fun getPokemonDetails(@Path("id") pokemonId: String): Call<PokemonDetailResponseDto>
 }
